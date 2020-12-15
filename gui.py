8215d5e42db38f09bd47730df7a49c9234ba484f
kumataro87 <74181353+kumataro87@users.noreply.github.com>
2020-12-16 02:08:59 +0900

    変更

--- gui.py
+++ gui.py
@@ -1,23 +1,37 @@
-import tkinter
-from tkinter import messagebox
-
-def button_click():
-    input_value = input_box.get()
-    messagebox.showinfo('クリックイベント',input_value + 'が入力されました')
-
+#ウインドウの作成
 root = tkinter.Tk()
-root.title('Python GUI')
-root.geometry('360x240')
+root.title("Python GUI")
+root.geometry("360x120")
 
-#入力蘭の作成
-input_box = tkinter.Entry(width=40)
-input_box.place(x=10, y=100)
+# 名前
+input_name_label = tkinter.Label(text="名前")
+input_name_label.grid(row=1, column=1, padx=10,)
 
-input_label =tkinter.Label(text="ラベル")
-input_label.place(x=10,y=70)
+# 入力欄の作成
+input_name = tkinter.Entry(width=40)
+input_name.grid(row=1, column=2)
 
-button = tkinter.Button(text='実行ボタン', command=button_click)
-button.place(x=10, y=130)
 
-#ウィンドウの描画
+# 住所
+input_address_label = tkinter.Label(text="住所")
+input_address_label.grid(row=2, column=1, padx=10,)
+
+# 住所入力欄の作成
+input_address = tkinter.Entry(width=40)
+input_address.grid(row=2, column=2)
+
+
+# 電話番号
+input_phone_label = tkinter.Label(text="電話番号")
+input_phone_label.grid(row=3, column=1, padx=10,)
+
+# 電話番号入力欄の作成
+input_phone = tkinter.Entry(width=40)
+input_phone.grid(row=3, column=2)
+
+#ボタンの作成
+button = tkinter.Button(text="実行ボタン",command=button_click)
+button.place(x=10, y=80)
+
+#ウインドウの描画
 root.mainloop()
